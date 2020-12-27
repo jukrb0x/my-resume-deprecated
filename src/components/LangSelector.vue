261af33b9cf3713fa1b0ef7c4fbf705750d0c689
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2020-12-27 20:21:29 +0800

    update i18n feature

---
+++ src/components/LangSelector.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-btn-dropdown color="primary" :label="currentLangName">
+    <q-list>
+      <q-item
+        v-for="lang in langOptions"
+        :key="lang.name"
+        clickable
+        v-close-popup
+        @click="switchLang(lang.code)"
+      >
+        <q-item-section>
+          <q-item-label>{{ lang.name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-btn-dropdown>
+</template>
+
+<script>
+export default {
+  name: 'LangSelector',
+  props: ['currentLangCode', 'switchLang'],
+  data() {
+    return {
+      langOptions: [
+        {
+          name: 'English',
+          code: 'en-us'
+        },
+        {
+          name: '简体中文',
+          code: 'zh-cn'
+        }
+      ]
+    };
+  },
+  computed: {
+    currentLangName: function() {
+      // get the name of lang code
+      if (this.currentLangCode === 'en-us') {
+        return 'English';
+      }
+      if (this.currentLangCode === 'zh-cn') {
+        return '简体中文';
+      }
+    }
+  },
+  methods: {}
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/mixins/LangSwitchMixin.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'LangSwitchMixin',
+  data() {
+    return {
+      currentLangCode: this.$i18n.locale
+    };
+  },
+  methods: {
+    switchLang(langCode) {
+      // switch the current language code and app i18n code
+      this.currentLangCode = langCode;
+      this.$i18n.locale = langCode;
+      // it is not elegant
+    }
+  }
+};
+</script>
