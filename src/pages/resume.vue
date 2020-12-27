12c2b3ab97af3b54d05ce15610687cfd4343cb14
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2020-12-28 04:00:48 +0800

    force update resume page

---
+++ src/pages/resume.vue
@@ -0,0 +1,194 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <div :class="mainCSS">
+      <!-- quick facts -->
+      <div class="quick-facts">
+        <!-- left -->
+        <div class="profilo">
+          <div class="avatar">
+            <img
+              src="https://avatars0.githubusercontent.com/u/15688641?s=460&u=96265432bd716f7af56c781d1022078f24bd91ae&v=4"
+              width="100px"
+              alt=""
+            />
+          </div>
+          <div class="lang-btn">
+            <LangSelector
+              :current-lang-code="currentLangCode"
+              :switch-lang="switchLang"
+            ></LangSelector>
+          </div>
+        </div>
+
+        <!-- medium -->
+        <div class="info">
+          <div class="facts">
+            <div class="name">{{ $t(`profile.name`) }}</div>
+            <div class="dept">{{ $t(`profile.dept`) }}</div>
+            <div class="uni">{{ $t(`profile.uni`) }}</div>
+          </div>
+          <div class="contact">
+            <!-- phone number -->
+            <q-chip
+              class="scale-up"
+              outline
+              icon="phone"
+              color="primary"
+              text-color="white"
+            >
+              {{ contact.tel }}
+              <q-tooltip :offset="[10, 10]">
+                {{ $t(`profile.phone`) }}
+              </q-tooltip>
+            </q-chip>
+
+            <!-- email -->
+            <q-chip
+              class="scale-up"
+              outline
+              icon="mail"
+              color="deep-orange"
+              text-color="white"
+            >
+              {{ contact.email }}
+              <q-tooltip :offset="[10, 10]">
+                {{ $t(`profile.email`) }}
+              </q-tooltip>
+            </q-chip>
+
+            <!-- website -->
+            <a href="https://wh0.is" target="_blank">
+              <q-chip
+                class="scale-up"
+                outline
+                icon="language"
+                color="red"
+                text-color="white"
+              >
+                {{ contact.website }}
+                <q-tooltip :offset="[10, 10]">
+                  {{ $t(`profile.website`) }}
+                </q-tooltip>
+              </q-chip>
+            </a>
+
+            <!-- github -->
+            <a href="https://github.com/jukrb0x" target="_blank">
+              <q-btn
+                class="scale-up"
+                flat
+                round
+                color="indigo"
+                icon="fab fa-github"
+              >
+                <q-tooltip :offset="[10, 10]">
+                  jukrb0x
+                </q-tooltip>
+              </q-btn>
+            </a>
+          </div>
+        </div>
+        <!-- right -->
+        <!--        <div class="contact">-->
+        <!--          <q-chip icon="phone">{{ contact.tel }}</q-chip>-->
+        <!--          <q-chip icon="mail">{{ contact.email }}</q-chip>-->
+        <!--          <q-chip icon="language">{{ contact.website }}</q-chip>-->
+        <!--        </div>-->
+      </div>
+
+      <div class="context">
+        <!-- Education -->
+        <li class="edu">
+          <dt class="title">
+            <q-icon name="school" size="sm"></q-icon>
+            {{ $t(`title.education`) }}
+          </dt>
+          <ul>
+            <li class="content">
+              <q-icon name="account_balance"></q-icon> {{ $t(`profile.uni`) }}
+            </li>
+          </ul>
+        </li>
+
+        <!-- Experience -->
+        <li class="exp">
+          <dt class="title">
+            <q-icon name="support" size="sm"></q-icon>
+            {{ $t(`title.experience`) }}
+          </dt>
+          <ul>
+            <li class="content">
+              123
+            </li>
+          </ul>
+        </li>
+
+        <!-- Research -->
+        <li class="rsch">
+          <dt class="title">
+            <q-icon name="science" size="sm"></q-icon>
+            {{ $t(`title.research`) }}
+          </dt>
+          <ul>
+            <li class="content">
+              12333
+            </li>
+          </ul>
+        </li>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import LangSwitchMixin from '../components/mixins/LangSwitchMixin';
+export default {
+  mixins: [LangSwitchMixin],
+  components: {
+    LangSelector: () => import('../components/LangSelector.vue')
+  },
+  data() {
+    return {
+      contact: {
+        tel: '+86 4008-823-823',
+        email: 'james.bond@007.com',
+        website: 'https://wh0.is'
+      }
+    };
+  },
+  mounted() {
+    console.log(this.$i18n);
+  },
+  methods: {},
+
+  computed: {
+    mainCSS: function() {
+      return this.currentLangCode === 'zh-cn' ? 'chinese-font main' : 'main';
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.main
+  background: #fff
+  padding: 25px
+  width: 1060px
+  box-shadow: 0 3px 18px 2px rgba(0, 0, 0, 0.14), 0 3px 1px -3px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12)
+.quick-facts
+  display: grid
+  grid-template-columns: 120px 600px
+  margin-bottom: 25px
+  .info
+    .facts
+      font-family: Georgia, "Microsoft Sans Serif"
+      .name
+        font-size: xx-large
+  .contact
+    margin-top: 5px
+    margin-left: -5px
+
+div.context > li > dt > i
+  padding-bottom: 3px
+</style>
