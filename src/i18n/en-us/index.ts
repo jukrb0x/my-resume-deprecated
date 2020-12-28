e89b060c98b349c42293297d23c84485d0ba7607
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2020-12-28 23:20:01 +0800

    fake fields of translation(i18n)

--- src/i18n/en-us/index.ts
+++ src/i18n/en-us/index.ts
@@ -1,9 +1,18 @@
 export default {
   profile: {
     name: 'James Bond',
-    dept: 'Software Engineering',
-    uni: 'Zoom University',
-    degree: 'Undergraduate B.E. Software Engineering',
+    motto:
+      'A license to kill',
+    interests:
+      'Driving faster and Kiss girls',
+    dept: 'double zero',
+    uni: 'MI6',
+    degree: 'Oxford?',
+    highSchool: {
+      degree: 'High School',
+      date: 'Sept 2016',
+      schoolName: 'abcde high school'
+    },
     phone: ' Phone Call',
     email: 'Working Email',
     website: 'Personal Blog'
@@ -11,6 +20,46 @@ export default {
   title: {
     education: 'Education',
     experience: 'Experience',
-    research: 'Research'
+    skills: 'Skills',
+    research: 'Projects & Research'
   },
+  present: 'Present',
+  skills: {
+    englishCert: 'English Certificate',
+    ielts: 'IELTS 9.0',
+    programming: 'Python / Vue.js / Typescript'
+  },
+  interest: 'Interests',
+  justfine: 'Just Fine/ Still learning',
+  graphery: {
+    date: 'Currently',
+    job: 'Frontend Developer',
+    title: 'HAHAHA',
+    description: '？？？？hahaha'
+  },
+  vac: {
+    date: 'Aug 2020 - Oct 2020',
+    job: 'MI6 Developer',
+    title: 'A???sadadasdasd',
+    description:
+      'what the heck'
+  },
+  proj: {
+    'my-resume': {
+      title: 'My Résumé',
+      description: 'A resume template powered by Vue.js and Quasar Framework'
+    },
+    daka: {
+      title: 'Daka',
+      description:
+        'A small Python script that helps student with daily health condition automated check-in in XMUM'
+    },
+    graphery: {
+      title: 'Graphery',
+      description:
+        'Graph Algorithm Tutorial Website for Computational Biology,\nI am focusing on the frontend design and user experience (UX), collaborated with Larry Zeng (Reed College)'
+    }
+  },
+  ghrepo: '项目仓库',
+  'exploring': 'On the way to explore...'
 };
