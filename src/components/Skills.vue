d66e3d7db1234887c425f7b74a72e18c12f51ac6
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2020-12-28 16:30:52 +0800

    add skills page

---
+++ src/components/Skills.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="ski">
+    <dt class="title">
+      <q-icon name="support" size="sm"></q-icon>
+      {{ $t(`title.skills`) }}
+    </dt>
+    <ul class="content">
+      <li>
+        123
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'Skills'
+};
+</script>
+
+<style scoped></style>
